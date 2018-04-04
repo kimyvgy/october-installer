90a495cf70e4376d78066b287bb7edc7e3055679
Nguyen Huu Kim <kimnguyen.ict@gmail.com>
2018-04-04 15:40:11 +0700

    Prepare writable directories

--- src/Installers/CLIInstaller.php
+++ src/Installers/CLIInstaller.php
@@ -4,7 +4,10 @@ namespace KimNH\OctoberInstaller\Console\Installers;
 
 use KimNH\OctoberInstaller\Console\Components\Downloader;
 use KimNH\OctoberInstaller\Console\Components\ZipExtract;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 
 class CLIInstaller
@@ -22,6 +25,8 @@ class CLIInstaller
         (new ZipExtract)
             ->removeSubDirectory('october-master')
             ->extract($zipFile, $directory);
+
+        $this->prepareWritableDirectories($directory, $io);
 
         $io->newLine();
         $io->section('<info>October is installed at: '.$directory.'</info>');
@@ -44,6 +49,26 @@ class CLIInstaller
                 $io->write($line);
             });
         }
+    }
+
+    /**
+     * Make sure the storage and bootstrap cache directories are writable.
+     *
+     * @param  string $appDirectory
+     * @param  \Symfony\Component\Console\Output\OutputInterface $output
+     * @return $this
+     */
+    protected function prepareWritableDirectories($appDirectory, OutputInterface $output)
+    {
+        $filesystem = new Filesystem();
+        try {
+            $filesystem->chmod($appDirectory.DIRECTORY_SEPARATOR."bootstrap/cache", 0755, 0000, true);
+            $filesystem->chmod($appDirectory.DIRECTORY_SEPARATOR."storage", 0755, 0000, true);
+        } catch (IOExceptionInterface $e) {
+            $output->writeln('<comment>You should verify that the "storage" and "bootstrap/cache" directories are writable.</comment>');
+        }
+
+        return $this;
     }
 
     /**
